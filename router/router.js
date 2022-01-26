ce53154e5e2d95e91bdae5389ccea9b35e11efa8
IMRANDIL <aliimranadil2@gmail.com>
2022-01-26 11:02:03 +0530

    yh

--- router/router.js
+++ router/router.js
@@ -21,7 +21,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
 
     const token = authHeader && authHeader.split(' ')[1];
-    console.log(token);
+
     if (token === null) {
         return res.sendStatus(401)
     }
