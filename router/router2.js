50a0804c3a7f7e46602d7be14f611ee0095a0172
IMRANDIL <aliimranadil2@gmail.com>
2022-01-26 12:46:40 +0530

    yh

--- router/router2.js
+++ router/router2.js
@@ -4,7 +4,30 @@ const router = require('express').Router();
 
 const jwt = require('jsonwebtoken');
 
+let refreshTokenn = []
 
+router.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokenn.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken })
+    })
+});
+
+
+
+
+//delete token....
+
+
+router.delete('/logout', (req, res) => {
+    refreshTokenn = refreshTokenn.filter(token => token !== req.body.token);
+
+    res.sendStatus(204)
+})
 
 
 router.post('/login', (req, res) => {
@@ -14,7 +37,8 @@ router.post('/login', (req, res) => {
     const user = { name: userName }
 
     const access_token = generateAccessToken(user)
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN);
+    refreshTokenn.push(refreshToken)
 
     res.json({ accessToken: access_token, refreshToken: refreshToken })
 
